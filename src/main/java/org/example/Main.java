cc2798c408caa64065783bf1598d6e14890263f2
prathapreddy6161 <110121153+prathapreddy6161@users.noreply.github.com>
2023-06-03 10:47:28 -0500

    helo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-
         System.out.println("Hello world!");
         System.out.println("this is for testing");
+        System.out.println("this is for sample");
 
     }
 }
