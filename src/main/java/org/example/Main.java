3b4600f56959082995d7abf6e9c6d6ff5755bf05
prathapreddy6161 <110121153+prathapreddy6161@users.noreply.github.com>
2023-06-03 10:49:28 -0500

    anil

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("this is for testing");
         System.out.println("this is for sample");
+        System.out.println("anil");
 
     }
 }
